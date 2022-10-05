73f519258710b677de327bae1269c15961856e8e
tamakachi <kenttechnical@gmail.com>
2022-10-05 16:44:52 +0200

    V-On Directive learned

--- main.js
+++ main.js
@@ -11,5 +11,13 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart++
+        },
+        changeImgSrc(variantSrc){
+            this.image = variantSrc
+        }
     }
 })
