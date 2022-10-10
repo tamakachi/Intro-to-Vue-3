fa35da8b740f7bc10aa57fe7002c6bb783d2ed04
tamakachi <kenttechnical@gmail.com>
2022-10-10 15:15:05 +0200

    Event emit challenge complete

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,7 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button v-on:click='removeFromCart'>Remove</button>
       </div>
     </div>
   </div>`,
@@ -56,10 +57,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart',this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart(){
+        this.$emit('remove-from-cart')
       }
   },
   computed: {
