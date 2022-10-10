9f48ec91f8e84551e5cdae1019ca6a13ed5a49e7
tamakachi <kenttechnical@gmail.com>
2022-10-10 14:54:08 +0200

    Components and props

---
+++ components/ProductDetails.js
@@ -0,0 +1,19 @@
+app.component('product-details',{
+    props:{
+        details:{
+            type: Array,
+            required:true
+        }
+    },
+    template:
+    /*html*/
+    `   <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>`,
+  data() {
+    return {
+        product: 'Socks',
+    }},
+methods: {},
+computed: {}
+})
